0fc98d92c05d0b86929ffff2df51075bb13b7d96
iamsoankit <98892759+iamsoankit@users.noreply.github.com>
2022-12-18 13:35:55 +0530

    Add files via upload

---
+++ Arrays/WAP to reverse the contents of an array of n elements.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+ 
+void main() 
+{
+    int a[100],reverse[100],i,n;
+ 
+    printf("\nEnter no of elements:\n");    
+    scanf("%d",&n);
+    
+    printf("Enter the array elements\n");
+    for (i=0;i<n;i++) 
+    {
+        scanf("%d",&a[i]);
+    }
+ 
+    for(i=0;i<n;i++)
+    {
+        reverse[i]=a[n-i-1];
+    }
+ 
+   printf("\nOn reversing the array elements we get\n");
+   for(i=0;i<n;i++) 
+   {
+      printf("%d ",reverse[i]);
+   }
+}
